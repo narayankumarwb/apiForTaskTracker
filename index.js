8dccb0b08372eff1a58d0d997c3a45d54b8f7ffb
narayankumarwb <152047155+narayankumarwb@users.noreply.github.com>
2024-03-06 21:28:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,203 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+connectDB();
+const taskSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    text: {
+      type: String,
+      required: [true, 'Please add a text value'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Task = mongoose.model('Task', taskSchema);
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a name'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const User = mongoose.model('User', userSchema);
+
+const app = express();
+// const corsOptions = {
+//   origin: 'https://tasktrackerjavascript.onrender.com', // frontend URL
+// };
+// app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// middleware to protect routes
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error('Not authorized');
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+
+app.post(
+  '/api/users/register',
+  asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      res.status(400);
+      throw new Error('Please add all fields');
+    }
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400);
+      throw new Error('User already exists');
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
+  })
+);
+
+app.post(
+  '/api/users/login',
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid credentials');
+    }
+  })
+);
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+// get all tasks
+app.get(
+  '/api/tasks',
+  protect,
+  asyncHandler(async (req, res) => {
+    const tasks = await Task.find({ user: req.user.id });
+    res.status(200).json(tasks);
+  })
+);
+
+// create a task
+app.post(
+  '/api/tasks',
+  protect,
+  asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+      res.status(400);
+      throw new Error('Please add a text field');
+    }
+    const task = await Task.create({
+      text: req.body.text,
+      user: req.user.id,
+    });
+    res.status(200).json(task);
+  })
+);
+
+// delete a task
+app.delete(
+  '/api/tasks/:id',
+  protect,
+  asyncHandler(async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      res.status(400);
+      throw new Error('Task not found');
+    }
+    if (!req.user) {
+      res.status(401);
+      throw new Error('User not found');
+    }
+    if (task.user.toString() !== req.user.id) {
+      res.status(401);
+      throw new Error('User not authorized');
+    }
+    await task.deleteOne();
+    res.status(200).json({ id: req.params.id });
+  })
+);
+
+app.listen(port, () => console.log(`API Server started on port ${port}`));
+
+module.exports = app;
